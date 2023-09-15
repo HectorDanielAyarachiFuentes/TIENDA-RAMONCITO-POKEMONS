9f5d9f5ba061d096fdce3ef2ec86e74113e13969
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-09-15 19:37:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ modoBoton.addEventListener('click', function() {
     // Alternar la clase "dark-mode" en el elemento body
     body.classList.toggle('dark-mode');
 });
+
+
+/** VENTANA EMERGENTE */
+// Obtén el elemento de la ventana emergente
+const miVentanaEmergente = document.getElementById('miVentanaEmergente');
+
+// Función para mostrar la ventana emergente
+function mostrarVentanaEmergente() {
+    miVentanaEmergente.style.display = 'block';
+}
+
+// Mostrar la ventana emergente automáticamente después de cinco segundos
+setTimeout(mostrarVentanaEmergente, 5000);
+
+// Obtén el elemento del botón para cerrar la ventana emergente
+const cerrarPopup = document.getElementById('cerrarPopup');
+
+// Función para cerrar la ventana emergente
+function cerrarVentanaEmergente() {
+    miVentanaEmergente.style.display = 'none';
+    miVentanaEmergente.scrollTop = 0;
+}
+
+// Agrega un evento de clic al botón para cerrar la ventana emergente
+cerrarPopup.addEventListener('click', cerrarVentanaEmergente);
